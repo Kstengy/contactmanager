3151b145897ebc33461ea96ffbfa44f5e55c51c1
kstengy <kstengy@gmail.com>
2019-03-05 16:21:14 -0600

    Updates

---
+++ precache-manifest.c27c040b2825e9baa42b5e337c702a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "2871bf012d9ae553ce10",
+    "url": "/contactmanager/static/js/main.2871bf01.chunk.js"
+  },
+  {
+    "revision": "c559b8e954f5c02f1c0e",
+    "url": "/contactmanager/static/js/2.c559b8e9.chunk.js"
+  },
+  {
+    "revision": "2871bf012d9ae553ce10",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "c559b8e954f5c02f1c0e",
+    "url": "/contactmanager/static/css/2.ebb52ada.chunk.css"
+  },
+  {
+    "revision": "0472a28a55ca194d8fa1b3ff5f07812f",
+    "url": "/contactmanager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kstengy.github.io/contactmanager/precache-manifest.dda5dc54d1cac298e5d38ea5fec41653.js"
+  "/contactmanager/precache-manifest.c27c040b2825e9baa42b5e337c702a0a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kstengy.github.io/contactmanager/index.html", {
+workbox.routing.registerNavigationRoute("/contactmanager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
